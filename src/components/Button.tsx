9c742cf85efda79075693099c20eb3cb172d5cb2
rdx.develops <rdx.develops@gmail.com>
2025-01-03 12:46:13 +0530

    feat: add a size prop to the button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,19 +7,24 @@ const classes = cva("border h-12 rounded-full px-6 font-medium", {
             primary: "bg-lime-400 text-neutral-950 border-lime-400",
             secondary: "border-white text-white bg-transparent",
         },
+        size: {
+            sm: "h-10",
+        },
     },
 });
 
 export default function Button(
     props: {
         variant: "primary" | "secondary";
+        size?: "sm";
     } & ButtonHTMLAttributes<HTMLButtonElement>
 ) {
-    const { variant, className, ...otherProps } = props;
+    const { variant, className, size, ...otherProps } = props;
     return (
         <button
             className={classes({
                 variant,
+                size,
                 className,
             })}
             {...otherProps}
